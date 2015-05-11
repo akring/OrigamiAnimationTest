ba873188a5360b0116988a9471213251b1a0782c
Akring <akring@163.com>
2015-05-11 13:50:30 +0800

    添加podfile

--- OrigamiAnimationTest/ViewController.m
+++ OrigamiAnimationTest/ViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "ViewController.h"
+#import <POP/POP.h>
+#import <POP/POPLayerExtras.h>
 
-@interface ViewController ()
+@interface ViewController (){
+    
+    BOOL scaled;
+}
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (nonatomic) CGFloat popAnimationProgress;
+
+@property (strong, nonatomic) UITapGestureRecognizer *tap;
 
 @end
 
@@ -16,8 +27,80 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    scaled = NO;
+    
+    _tap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                   action:@selector(tapHandler)];
+    
+    //开启imageView的用户交互选项，否则imageView不会响应手势
+    self.imageView.userInteractionEnabled = YES;
+    
+    [self.imageView addGestureRecognizer:_tap];
 }
+
+
+- (void)tapHandler{
+    
+    if (scaled) {
+        
+        [self togglePopAnimation:NO];
+        
+        scaled = !scaled;
+    }
+    else{
+        
+        [self togglePopAnimation:YES];
+        
+        scaled = !scaled;
+    }
+}
+
+// popAnimation transition
+
+- (void)togglePopAnimation:(BOOL)on {
+    POPSpringAnimation *animation = [self pop_animationForKey:@"popAnimation"];
+    
+    if (!animation) {
+        animation = [POPSpringAnimation animation];
+        animation.springBounciness = 10;
+        animation.springSpeed = 6;
+        animation.property = [POPAnimatableProperty propertyWithName:@"popAnimationProgress" initializer:^(POPMutableAnimatableProperty *prop) {
+            prop.readBlock = ^(ViewController *obj, CGFloat values[]) {
+                values[0] = obj.popAnimationProgress;
+            };
+            prop.writeBlock = ^(ViewController *obj, const CGFloat values[]) {
+                obj.popAnimationProgress = values[0];
+            };
+            prop.threshold = 0.001;
+        }];
+        
+        [self pop_addAnimation:animation forKey:@"popAnimation"];
+    }
+    
+    animation.toValue = on ? @(1.0) : @(0.0);
+}
+
+- (void)setPopAnimationProgress:(CGFloat)progress {
+    _popAnimationProgress = progress;
+    
+    CGFloat transition1 = POPTransition(progress, 0, 360);
+    POPLayerSetRotationZ(self.imageView.layer, POPDegreesToRadians(transition1));
+    
+    CGFloat transition2 = POPTransition(progress, 1, 2);
+    POPLayerSetScaleXY(self.imageView.layer, CGPointMake(transition2, transition2));
+}
+
+// Utilities
+
+static inline CGFloat POPTransition(CGFloat progress, CGFloat startValue, CGFloat endValue) {
+    return startValue + (progress * (endValue - startValue));
+}
+
+static inline CGFloat POPDegreesToRadians(CGFloat degrees) {
+    return M_PI * (degrees / 180.0);
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
